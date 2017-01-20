069369b57f808114421b60e2c6473a6a13f2afd6
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-01-20 21:27:19 +0800

    add filter to extract invalid data

--- CSVData.m
+++ CSVData.m
@@ -11,8 +11,8 @@ classdef CSVData < handle
     
     methods
         function obj = CSVData(varargin)
-            if nargin == 2
-                obj = CSVData.parse(varargin{1},varargin{2});
+            if nargin == 3
+                obj = CSVData.parse(varargin{1},varargin{2},varargin{3});
             else if nargin == 7
                     obj.collectTime = varargin{1};
                     obj.specularType = varargin{2};
@@ -60,10 +60,15 @@ classdef CSVData < handle
                 pks = 0;
             end
         end
+        
+        function [a,b] = getMassRange(obj)
+            a = obj.mass(1);
+            b = obj.mass(end);
+        end
     end
 
     methods (Static)
-         function csvData = parse(folderPath,fileName)
+         function csvData = parse(folderPath,fileName,minMS2Ratio)
              strArray = split(fileName,'.');
              tmp = strArray(1);
              fileNum = str2num(tmp{1});
@@ -71,15 +76,27 @@ classdef CSVData < handle
              tmpStr = fgets(fid);
              strArray = split(tmpStr,',');
              time = str2double(strArray{1});
+             minMS2 = 0;
              if strcmp(strArray{4},'MS2')
                  stype = MassSpecularType.MS2;
                  parentMass = str2double(strArray{5});
+                 if and(minMS2Ratio > 0,minMS2Ratio <= 1)
+                     minMS2 = parentMass * minMS2Ratio;
+                 else if minMS2Ratio > 1
+                         minMS2 = minMS2Ratio;
+                     end
+                 end
              else
                  stype = MassSpecularType.MS1;
                  parentMass = 0;
              end
              mass = str2double(split(fgets(fid),','));
              massIntensity = str2double(split(fgets(fid),','));
+             if stype == MassSpecularType.MS2
+                 filter = or(mass < minMS2, mass > parentMass);
+                 massIntensity(filter) = [];
+                 mass(filter) = [];
+             end
              maxI = max(massIntensity);
              fclose(fid);
              csvData = CSVData(time,stype,maxI,mass,massIntensity,parentMass,fileNum);
